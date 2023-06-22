9c5ae2650605c012e7d893fd0fa08deb19a18c61
MaBi0110 <biziewskamk@gmail.com>
2023-06-22 19:06:44 +0200

    add userNavigationTemplate.xhtml

--- src/main/java/com/example/models/Person.java
+++ src/main/java/com/example/models/Person.java
@@ -3,6 +3,9 @@ package com.example.models;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Getter
 @Setter
 public class Person {
@@ -13,6 +16,7 @@ public class Person {
     private String login;
     private String pass;
     private String accountType;
+    private List<Book> borrowedBooks = new ArrayList<>();
 
     public Person(String firstName, String lastName, String login, String pass, String accountType) {
         this.firstName = firstName;
